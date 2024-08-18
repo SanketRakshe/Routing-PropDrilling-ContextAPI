f8eebb50bf95b68235972068680cebad631becad
sanketrakshe <sanketrakshe07@gmail.com>
2024-08-18 14:39:33 +0530

    Lazy loading implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,82 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Landing } from "./components/Landing";
-import { Dashboard } from "./components/Dashboard";
-import { useNavigate } from "react-router-dom";
+// import Home from "./components/Home";
+// import { Landing } from "./components/Landing";
+// import { Dashboard } from "./components/Dashboard";
+// import { useNavigate } from "react-router-dom";
+
+import React, { Suspense } from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+
+// Lazy load the components
+const Home = React.lazy(() => import('./components/Home'));
+const About = React.lazy(() => import('./components/About'));
+
+
+// Routing................................................................
+
+// function App() {
+//   return (
+//     <>
+//       <BrowserRouter>
+//       <AppBar />
+//         <Routes>
+//           <Route path="/" element={<Landing />} />
+//           <Route path="/dashboard" element={<Dashboard />} />
+//         </Routes>
+//       </BrowserRouter>
+//     </>
+//   )
+// }
+
+
+// function AppBar() {
+//   const navigate = useNavigate();
+//   return (
+//     <div style={{background: "black"}}>
+//         <h1 style={{color: "white"}}>This is a Navbar</h1>
+
+//         <button onClick={() => {
+//           //window.location.href = "/"
+//           navigate("/");
+//         }}>Landing</button>
+
+//         <button onClick={() => {
+//           //window.location.href = "/dashboard"
+
+//           navigate("/dashboard");
+//         }}>Dashboard</button>
+//       </div>
+//   )
+// }
+
+
+// ................................................................
+// Lazy loading
+// ................................................................
+
+
 
 function App() {
   return (
-    <>
-      <BrowserRouter>
-      <AppBar />
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Suspense fallback={<div>Loading...</div>}>
         <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
         </Routes>
-      </BrowserRouter>
-    </>
-  )
+      </Suspense>
+    </Router>
+  );
 }
-
-
-function AppBar() {
-  const navigate = useNavigate();
-  return (
-    <div style={{background: "black"}}>
-        <h1 style={{color: "white"}}>This is a Navbar</h1>
-
-        <button onClick={() => {
-          //window.location.href = "/"
-          navigate("/");
-        }}>Landing</button>
-
-        <button onClick={() => {
-          //window.location.href = "/dashboard"
-
-          navigate("/dashboard");
-        }}>Dashboard</button>
-      </div>
-  )
-}
-
 
 export default App;
---
+++ src/components/About.jsx
@@ -0,0 +1,9 @@
+function About() {
+    return (
+        <>
+            <div> This is About componnet...</div>
+        </>
+    )
+}
+
+export default About;
